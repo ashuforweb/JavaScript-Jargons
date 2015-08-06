4d6fe4f481acfb94d3053a5275941b3048dd0e68
Ashutosh Singh <ashuforweb@gmail.com>
2015-08-05 23:38:23 -0700

    v0.01g

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-document.getElementById('btntest').addEventListener('click',fnTest,false);
+document.getElementById('btntest').addEventListener('click', fnTest, false);
 function fnTest () {
-  console.log('This function have '+arguments.length+' Arguments');
+  console.log('This function have ' + arguments.length + ' Arguments');
 }
+
+function fnPerson (firstName, secondName) {
+  this.firstName = firstName;
+  this.secondName = secondName;
+}
+
+var Saad = new fnPerson('Ashutosh', 'Singh');
+console.log(Saad.firstName + Saad.secondName);
